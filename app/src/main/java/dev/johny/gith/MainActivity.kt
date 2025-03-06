81cdf05e4546c7ce8ad4bd6900f8116379ec404f
Evgenii L <evgenij.luk11@gmail.com>
2025-03-06 13:21:01 +0700

    Added companion for create intent in FeatureActivity

--- app/src/main/java/dev/johny/gith/MainActivity.kt
+++ app/src/main/java/dev/johny/gith/MainActivity.kt
@@ -1,17 +1,17 @@
 package dev.johny.gith
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import dev.johny.empty_feature.FeatureActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, Class.forName("dev.johny.empty_feature.FeatureActivity"))
         val button = findViewById<Button>(R.id.goToFeature)
         button.setOnClickListener {
+            val intent = FeatureActivity.createIntent(this)
             startActivity(intent)
         }
     }
