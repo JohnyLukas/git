03b2436a3720624a64ce5e610cdbe08654e86221
Evgenii L <evgenij.luk11@gmail.com>
2025-03-06 15:13:40 +0700

    Removed startActivity call in MainActivity

--- app/src/main/java/dev/johny/gith/MainActivity.kt
+++ app/src/main/java/dev/johny/gith/MainActivity.kt
@@ -3,8 +3,6 @@ package dev.johny.gith
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import dev.johny.empty_feature.EmptyFeatureActivity
-import dev.johny.second_feature.SecondFeatureActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +12,8 @@ class MainActivity : AppCompatActivity() {
         val goToEmptyFeatureButton = findViewById<Button>(R.id.goMainToEmptyFeature)
         val goToSecondFeatureButton = findViewById<Button>(R.id.goMainToSecondFeature)
 
-        goToEmptyFeatureButton.setOnClickListener {
-            val intent = EmptyFeatureActivity.getIntent(this)
-            startActivity(intent)
-        }
+        goToEmptyFeatureButton.setOnClickListener {}
 
-        goToSecondFeatureButton.setOnClickListener {
-            val intent = SecondFeatureActivity.getIntent(this)
-            startActivity(intent)
-        }
+        goToSecondFeatureButton.setOnClickListener {}
     }
 }
