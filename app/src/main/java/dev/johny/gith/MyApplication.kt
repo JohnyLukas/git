94831a83fd20894215cc4501b10828137f6e763e
Evgenii L <evgenij.luk11@gmail.com>
2025-03-07 17:29:53 +0700

    Added network module

--- app/src/main/java/dev/johny/gith/MyApplication.kt
+++ app/src/main/java/dev/johny/gith/MyApplication.kt
@@ -5,15 +5,22 @@ import dev.johny.api_empty_feature.EmptyFeatureMediator
 import dev.johny.api_empty_feature.EmptyFeatureMediatorProvider
 import dev.johny.api_second_feature.SecondFeatureMediator
 import dev.johny.api_second_feature.SecondFeatureMediatorProvider
+import dev.johny.core_network.RetrofitProviderImpl
+import dev.johny.core_network_api.RetrofitProvider
 import dev.johny.empty_feature.EmptyFeatureMediatorImpl
 import dev.johny.second_feature.SecondFeatureMediatorImpl
+import retrofit2.Retrofit
 
-class MyApplication : Application(), EmptyFeatureMediatorProvider, SecondFeatureMediatorProvider {
+class MyApplication : Application(), EmptyFeatureMediatorProvider, SecondFeatureMediatorProvider, RetrofitProvider {
     override fun getEmptyFeatureMediator(): EmptyFeatureMediator {
         return EmptyFeatureMediatorImpl()
     }
 
     override fun getSecondFeatureMediator(): SecondFeatureMediator {
         return SecondFeatureMediatorImpl()
+    }
+
+    override fun provideRetrofit(baseUrl: String): Retrofit {
+        return RetrofitProviderImpl().provideRetrofit(baseUrl)
     }
 }
