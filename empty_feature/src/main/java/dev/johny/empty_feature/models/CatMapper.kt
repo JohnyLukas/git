b4af605bc533812670abe3da1fede095468f68b1
Evgenii L <evgenij.luk11@gmail.com>
2025-03-11 15:08:06 +0700

    Fixes

--- empty_feature/src/main/java/dev/johny/empty_feature/models/CatMapper.kt
+++ empty_feature/src/main/java/dev/johny/empty_feature/models/CatMapper.kt
@@ -1,15 +1,17 @@
 package dev.johny.empty_feature.models
 
+private const val protectedProtocol = "https://"
+private const val unprotectedProtocol = "http://"
+
 class CatMapper {
     fun checkProtocolOnImageUrl(cat: CatResponseItem): CatResponseItem {
-        val protectedProtocol = "https://"
-        val unprotectedProtocol = "http://"
         val isUrlProtected = cat.url.startsWith(protectedProtocol)
 
-        return cat.copy(
-            url = if (isUrlProtected) cat.url
-            else cat.url.replaceFirst(unprotectedProtocol, protectedProtocol)
-        )
+        return if (isUrlProtected) {
+            cat
+        } else {
+            cat.copy(url = cat.url.replaceFirst(unprotectedProtocol, protectedProtocol))
+        }
     }
 
 }
